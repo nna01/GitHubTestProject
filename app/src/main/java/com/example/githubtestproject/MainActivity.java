5a55acca0caaa15e6b5a4735418ed438618ed3eb
Nowshin Nawar Arony <n.nawar94@yahoo.com>
2020-02-15 16:00:59 +0600

    Changed Text and Color to 'Github' and Ivory

--- app/src/main/java/com/example/githubtestproject/MainActivity.java
+++ app/src/main/java/com/example/githubtestproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.githubtestproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Displaying Toast with Hello World message
+        Toast.makeText(getApplicationContext(),"Hello Git",Toast.LENGTH_SHORT).show();
     }
 }
